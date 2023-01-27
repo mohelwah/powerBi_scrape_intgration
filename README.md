99404836e0f038e8308c5fc8e147cfab9f3a2ba9
Mohamed Abdelwahab <moh.elwah@gmail.com>
2023-01-27 23:47:35 +0200

    prepare the project form my lab

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
-# PythonScaffoldTemplate
+# Scrape with power bi integeration
 this is scaffold template for python 
  Create the following:
  - Makefile
  - requirement.txt
- - virtial enviroment: python -m venv ~/.scaffold
- - activate venv envi: source ~/.scaffold/bin/activate
+ -Linux system:
+    - virtial enviroment: python -m venv ~/.scrapePowerbi
+    - activate venv envi: source ~/.scrapePowerbi/bin/activate
+ - Windows system:
+    - virtial enviroment: python -m venv c:\venv\scrapePowerbi
+    - activate venv envi:   C:\venv\scrapePowerbi\Scripts\activate.ps1
  - hello.py file
  - test_hello.py file 
+ - to check the python version:
+    - in linux : which python  
+    - in windows: Get-Command python | fl *
+ - get pack list:
+    - pip freeze or pip list
--- main.py
+++ main.py
@@ -7,17 +7,19 @@ from time import sleep
 from random import randint
 import pandas as pd
 
-executable_path='C:\webdriver\chromedriver.exe'
+executable_path = "C:\webdriver\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=executable_path)
-driver.get('https://www.realestate.moj.gov.kw/live/Moj_Rs_11.aspx')
+driver.get("https://www.realestate.moj.gov.kw/live/Moj_Rs_11.aspx")
+
 
 def small_sleep(low=1, high=10):
-    sleep(randint(low,high))
+    sleep(randint(low, high))
 
 
 def large_sleep(low=1, high=20):
-    sleep(randint(low,high))
-    
+    sleep(randint(low, high))
+
+
 def get_data(driver):
     data = {}
     elements = driver.find_elements(By.XPATH("//*"))
@@ -32,29 +34,32 @@ def check_button(driver, id):
     if not element.is_selected():
         element.click()
 
+
 def click_button(driver, id):
     element = driver.find_element(By.ID(id))
     element.click()
 
+
 def get_beautiful_soup(driver):
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    soup = BeautifulSoup(driver.page_source, "html.parser")
     return soup
 
 
 def get_table_data(driver):
     source = get_beautiful_soup(driver)
-    table = source.find('table')
-    table_body = table.find('tbody')
-    rows = table_body.find_all('tr')
+    table = source.find("table")
+    table_body = table.find("tbody")
+    rows = table_body.find_all("tr")
     for row in rows:
-        cols = row.find_all('td')
+        cols = row.find_all("td")
         cols = [ele.text.strip() for ele in cols]
-        data.append([ele for ele in cols if ele]) # Get rid of empty values
+        data.append([ele for ele in cols if ele])  # Get rid of empty values
     return True
-    
 
 
-from_date = '5/1/2023'
+# driver.find_element_by_link_text(“Testsigma Cloud”).click()
+
+from_date = "5/1/2023"
 to_date = "23/01/2023"
 
 from_date_el = driver.find_element(By.ID, "bodyContent_fromDate")
@@ -66,7 +71,7 @@ small_sleep()
 ## click to main button
 checked = False
 
-if checked== False:
+if checked == False:
     driver.execute_script('document.getElementById("bodyContent_cbGov").click()')
     small_sleep()
     driver.execute_script('document.getElementById("bodyContent_cbZone").click()')
@@ -74,10 +79,10 @@ if checked== False:
     driver.execute_script('document.getElementById("bodyContent_cbCat").click()')
     small_sleep()
     checked = True
-    
-if checked==True:
+
+if checked == True:
     driver.execute_script('document.getElementById("bodyContent_btnSubmit").click()')
-    
+
 source = get_beautiful_soup(driver)
 
 
