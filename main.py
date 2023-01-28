44875f8e39c4dc3d6c962d81de91ef2160fa8ade
Mohamed Abdelwahab <moh.elwah@gmail.com>
2023-01-28 15:50:34 +0200

    add webdriver manger and chromdriver auto install

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from time import sleep
 from random import randint
 import pandas as pd
 
-
+'''
 ## this for github mode
 
 from selenium import webdriver
@@ -36,6 +36,55 @@ driver = webdriver.Chrome(options=chrome_options)
 
 # executable_path = "C:\webdriver\chromedriver.exe"
 # driver = webdriver.Chrome(executable_path=executable_path)
+'''
+
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import chromedriver_autoinstaller
+
+import requests # 2
+import json # 3
+
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(800, 800))  
+display.start()
+
+chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+                                      # and if it doesn't exist, download it automatically,
+                                      # then add chromedriver to path
+
+chrome_options = webdriver.ChromeOptions()    
+# Add your options as needed    
+options = [
+  # Define window size here
+   "--window-size=1200,1200",
+    "--ignore-certificate-errors"
+ 
+    #"--headless",
+    #"--disable-gpu",
+    #"--window-size=1920,1200",
+    #"--ignore-certificate-errors",
+    #"--disable-extensions",
+    #"--no-sandbox",
+    #"--disable-dev-shm-usage",
+    #'--remote-debugging-port=9222'
+]
+
+for option in options:
+    chrome_options.add_argument(option)
+
+    
+driver = webdriver.Chrome(options = chrome_options)
+
+
+
 driver.get("https://www.realestate.moj.gov.kw/live/Moj_Rs_11.aspx")
 
 
